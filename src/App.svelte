e4ffffe37e2d5b660181d7cdadc3d3cf17b375bb
codeconcept <contact@codeconcept.fr>
2019-06-09 14:24:55 +0200

    iterating over all messages

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,42 @@
 <script>
-	export let name;
-	let message = "inital message value";
+  export let name;
+  let message = "";
+  let messages = [];
 
-	function updateMessage(event) {
-		message = event.target.value;
-	}
+  function updateMessage(event) {
+    message = event.target.value;
+  }
+
+  function saveMessage() {
+    const newMessage = {
+      id: Date.now(),
+      text: message,
+      author: "Sam"
+    };
+    messages = [newMessage, ...messages];
+  }
 </script>
 
 <style>
-	h1 {
-		color: rgb(89, 144, 216);
-	}
+  h1 {
+    color: rgb(89, 144, 216);
+  }
 </style>
 
 <h1>Hello {name}!</h1>
-<textarea cols="50" rows="5" value={message} on:input={updateMessage}></textarea>
+<textarea cols="50" rows="5" on:input={updateMessage} />
+<br />
+<button on:click={saveMessage}>send</button>
 <div>
   <h3>Preview</h3>
-  {message}
+   {message}
+</div>
+
+<div>
+  <h2>Messages</h2>
+  {#each messages as message}
+    <div>By {message.author}</div>
+    <div> {message.text} </div>
+    <hr />
+  {/each}
 </div>
