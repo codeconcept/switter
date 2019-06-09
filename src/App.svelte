76c25a136bb276e072b676ffcb5e052fffd40e31
codeconcept <contact@codeconcept.fr>
2019-06-09 14:41:18 +0200

    cleaning

--- src/App.svelte
+++ src/App.svelte
@@ -3,22 +3,10 @@
   export let name;
   let message = "";
   let messages = [];
-
-  function saveMessage() {
-    const newMessage = {
-      id: Date.now(),
-      text: message,
-      author: "Sam"
-    };
-    messages = [newMessage, ...messages];
-    message = "";
-  }
 </script>
 
 <style>
-  h1 {
-    color: rgb(89, 144, 216);
-  }
+
 </style>
 
 <Message />
