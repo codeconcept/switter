3711557c27bad8998edf7d103c42140b846475c8
codeconcept <contact@codeconcept.fr>
2019-06-09 14:10:44 +0200

    listening to input event

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let name;
+	let message = "inital message value";
+
+	function updateMessage(event) {
+		console.log('updateMessage', event.target.value);
+	}
+</script>
+
+<style>
+	h1 {
+		color: rgb(89, 144, 216);
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+<textarea cols="50" rows="5" value={message} on:input={updateMessage}></textarea>
+<div>
+  {message}
+</div>
